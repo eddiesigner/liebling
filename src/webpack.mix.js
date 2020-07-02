c86a756ab7b8217fa92d8a7dfb8b0ef76a19f7ab
Eduardo Gomez <this.eduardo@gmail.com>
2020-07-02 12:40:13 +0200

    Re-organize more stylesheets

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -26,7 +26,9 @@ mix.js('js/helpers.js', 'js/')
   .setPublicPath('../assets')
   .sass('sass/app.scss', 'css/')
   .sass('sass/home.scss', 'css/')
+  .sass('sass/listing.scss', 'css/')
   .sass('sass/post.scss', 'css/')
+  .sass('sass/newsletter.scss', 'css/')
   .sass('sass/404.scss', 'css/')
   .options({
     processCssUrls: false
