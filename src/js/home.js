42d726ec5f7eef1589c2f958988a0eb6bf4961e2
Eduardo Gómez <this.eduardo@gmail.com>
2020-07-07 17:13:29 +0200

    Improve accessibility (#221)

--- src/js/home.js
+++ src/js/home.js
@@ -13,7 +13,7 @@ $(document).ready(() => {
   if ($featuredSlider.length > 0) {
     const numSlides = $featuredSlider.find('.js-featured-slide').length
     const featuredSlider = new Glide('.js-featured-slider', {
-      type: numSlides === 1 ? 'slider' : 'carousel',
+      type: 'slider',
       rewind: false,
       gap: 0,
       swipeThreshold: false,
@@ -27,15 +27,63 @@ $(document).ready(() => {
       }
     })
 
+    const ArrowDisabler = (Glide, Components) => {
+      const controlClasses = {
+        'controls': 'js-featured-slider-controls',
+        'backArrow': 'js-featured-slider-previous',
+        'nextArrow': 'js-featured-slider-next',
+      }
+
+      return {
+        mount() {
+          if (Glide.settings.rewind || numSlides === 1) {
+            return
+          }
+
+          Glide.on(['mount.after', 'run'], () => {
+            for (let controlItem of Components.Controls.items) {
+              if (!controlItem.className.includes(controlClasses.controls)) {
+                continue
+              }
+
+              const left = controlItem.querySelector(`.${controlClasses.backArrow}`)
+
+              if (left) {
+                if (Glide.index === 0) {
+                  left.setAttribute('disabled', '')
+                } else {
+                  left.removeAttribute('disabled')
+                }
+              }
+
+              const right = controlItem.querySelector(`.${controlClasses.nextArrow}`)
+
+              if (right) {
+                const lastSlideIndex = Glide.settings.bound
+                  ? Glide.index + (Glide.settings.perView - 1)
+                  : Glide.index
+
+                if (lastSlideIndex === Components.Sizes.length - 1) {
+                  right.setAttribute('disabled', '')
+                } else {
+                  right.removeAttribute('disabled')
+                }
+              }
+            }
+          })
+        }
+      }
+    }
+
     if (numSlides === 1) {
-      $featuredSlider.find('.js-controls').remove()
+      $featuredSlider.find('.js-featured-slider-controls').remove()
     }
 
     featuredSlider.on('mount.after', () => {
       shave('.js-featured-article-title', 200)
     })
     
-    featuredSlider.mount({ Controls, Swipe, Breakpoints })
+    featuredSlider.mount({ Controls, Swipe, Breakpoints, ArrowDisabler })
 
     setTimeout(() => {
       window.dispatchEvent(new Event('resize'))
